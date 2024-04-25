ea8489777692f9e7fbabc5c18afe0c9794cef24a
Alysson <alysouza@gmail.com>
2024-04-25 19:07:01 -0300

    Atualização Funcionário.

--- src/edu/up/br/restaurante/FileManager.java
+++ src/edu/up/br/restaurante/FileManager.java
@@ -1,4 +1,29 @@
 package edu.up.br.restaurante;
 
-public class FileManager {
+import java.io.File;
+import java.io.IOException;
+
+public class FileManager
+{
+    String pathNAme = "C:\\Users\\alysouza\\Documents\\GitHub\\java-projects\\src\\empresa\\up\\edu\\br\\Root";
+
+    public void criarDiretorio()
+    {
+        File diretorio = new File(pathNAme);
+        if (!diretorio.exists())
+        {
+            boolean statusDir = diretorio.mkdir();
+            System.out.println(statusDir);
+        }
+    }
+
+    public File criarArquivo() throws IOException {
+        File arquivo = new File(pathNAme, "alunos.txt");
+        if (!arquivo.exists())
+        {
+            boolean statusArq = arquivo.createNewFile();
+            System.out.println(statusArq);
+        }
+        return arquivo;
+    }
 }
--- src/edu/up/br/restaurante/Funcionario.java
+++ src/edu/up/br/restaurante/Funcionario.java
@@ -1,4 +1,47 @@
 package edu.up.br.restaurante;
 
 public class Funcionario {
+
+    private static int count = 0;
+    private int id_funcionario;
+    private String nome;
+    private String cargo;
+
+    public Funcionario(int id_funcionario, String nome, String cargo)
+    {
+        this.id_funcionario = ++count;
+        this.nome = nome;
+        this.cargo = cargo;
+    }
+
+    public int getId_funcionario() {
+        return id_funcionario;
+    }
+
+    public void setId_funcionario(int id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void cadastrarFuncionario()
+    {
+        System.out.println();
+
+
+    }
 }
